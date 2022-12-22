dd7fae836aadc6e3f6c2e5c7c7d27d5881257f7e
Anton Shefer <rezajkee@mail.ru>
2022-12-22 13:48:24 +0500

    Add CRUD courses urls and templates, required mixins

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
@@ -18,7 +22,7 @@ class OwnerEditMixin:
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ["subject", "title", "slug", "overview"]
     success_url = reverse_lazy("manage_course_list")
@@ -30,15 +34,17 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = "courses.add_course"
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = "courses.change_course"
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
