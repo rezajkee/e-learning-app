218e9f70b94ce0dbcb5d0809a11396ba00944e2f
Anton Shefer <rezajkee@mail.ru>
2022-12-26 17:29:57 +0500

    Change course enroll view to action of course view set, add custom permission 'IsEnroll'

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..models import Course, Subject
-from .serializers import SubjectSerializer
+from .serializers import CourseSerializer, SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -19,11 +20,27 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+# class CourseEnrollView(APIView):
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({"enrolled": True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
