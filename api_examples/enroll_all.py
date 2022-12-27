3bb7d8f5b5d11eecccf35790f921c67ea1e4a97b
Anton Shefer <rezajkee@mail.ru>
2022-12-27 14:45:40 +0500

    Add some API usage examples

---
+++ api_examples/enroll_all.py
@@ -0,0 +1,26 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
+
+
+username = "rez789"
+password = os.getenv("password")
+base_url = "http://127.0.0.1:8000/api/"
+
+
+# Retrieve all courses
+r = requests.get(f"{base_url}courses/")
+courses = r.json()
+available_courses = ", ".join([course["title"] for course in courses])
+print(f"Available courses: {available_courses}")
+for course in courses:
+    course_id = course["id"]
+    course_title = course["title"]
+    r = requests.post(
+        f"{base_url}courses/{course_id}/enroll/", auth=(username, password)
+    )
+    if r.status_code == 200:
+        print(f"Successfully enrolled in '{course_title}'")
