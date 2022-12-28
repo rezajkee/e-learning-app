925862c4a5863e5cf8f3b61fd2093b10e16a3bb3
Anton Shefer <rezajkee@mail.ru>
2022-12-28 21:42:01 +0500

    Add broadcasting messages via Redis

--- educa/settings.py
+++ educa/settings.py
@@ -182,3 +182,12 @@ REST_FRAMEWORK = {
 # Channels settings
 
 ASGI_APPLICATION = "educa.asgi.application"
+# Redis
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
