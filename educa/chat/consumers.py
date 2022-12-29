7e6857c9b222ba4c3ba1797c09be015898da6d86
Anton Shefer <rezajkee@mail.ru>
2022-12-29 13:27:15 +0500

    Add message sender's username and localtime to messages

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,11 +1,13 @@
 import json
 
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.id}"
         # Join room group
@@ -25,12 +27,15 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        now = timezone.now()
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 "type": "chat_message",
                 "message": message,
+                "user": self.user.username,
+                "datetime": now.isoformat(),
             },
         )
 
